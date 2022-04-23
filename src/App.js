6d01dfb3b93402e9f7936cfde40e6a82dcb533ff
Hoat Ha <>
2022-04-23 19:46:40 +0700

    Update content

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+const requestOptions = {
+  method: "GET",
+  headers: { "Content-Type": "application/json" },
+};
 
 function App() {
+  const [bookStructuredData, setBooKStructuredData] = useState({});
+  useEffect(() => {
+    fetch("https://truyenconect.com/api/story/get/101", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setBooKStructuredData(data);
+      });
+  });
+
   return (
     <div className="App">
+      <script type="application/ld+json">
+        {JSON.stringify(bookStructuredData)}
+      </script>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {bookStructuredData && bookStructuredData.image ? (
+          <img src={bookStructuredData.image} className="App-logo" alt="logo" />
+        ) : null}
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <div
+            dangerouslySetInnerHTML={{ __html: bookStructuredData.short_desc }}
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
