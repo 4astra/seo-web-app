d8a2314ecb8a1f13082afde5feed03a8e803e082
Hoat <brian.ha289@gmail.com>
2022-04-23 20:17:42 +0700

    Add meta data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 const requestOptions = {
   method: "GET",
   headers: { "Content-Type": "application/json" },
@@ -7,29 +8,50 @@ const requestOptions = {
 
 function App() {
   const [bookStructuredData, setBooKStructuredData] = useState({});
+  const [bookSEO, setBookSEO] = useState({});
+
   useEffect(() => {
     fetch("https://truyenconect.com/api/story/get/101", requestOptions)
       .then((response) => response.json())
       .then((data) => {
         setBooKStructuredData(data);
+        if (data) {
+          setBookSEO({
+            "@context": "https://schema.org/",
+            "@type": "Book",
+            bookFormat: "http://schema.org/EBook",
+            name: data.title,
+            image: data.image,
+            author: {
+              "@type": "Thing",
+              name: data.author.name,
+            },
+            datePublished: data.updated,
+            description: data.short_desc,
+          });
+        }
       });
   });
 
   return (
     <div className="App">
-      <script type="application/ld+json">
-        {JSON.stringify(bookStructuredData)}
-      </script>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{bookStructuredData.title}</title>
+        <meta name="keyword" content={bookStructuredData.meta_keyword} />
+        <meta name="description" content={bookStructuredData.meta_desc} />
+      </Helmet>
+      {bookSEO ? (
+        <script type="application/ld+json">{JSON.stringify(bookSEO)}</script>
+      ) : null}
       <header className="App-header">
         {bookStructuredData && bookStructuredData.image ? (
           <img src={bookStructuredData.image} className="App-logo" alt="logo" />
         ) : null}
 
-        <p>
-          <div
-            dangerouslySetInnerHTML={{ __html: bookStructuredData.short_desc }}
-          />
-        </p>
+        <div
+          dangerouslySetInnerHTML={{ __html: bookStructuredData.short_desc }}
+        />
       </header>
     </div>
   );
